772bb08d947b779fa15f2f1644145efb73cfd3e7
leel_1107 <eonletian1107@gmail.com>
2026-07-21 17:49:41 +0800

    Change appstore link

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,11 +9,10 @@ export const environment = {
   androidAppVersion: '1.3.6',
   iosAppVersion: '1.3.6',
   androidStore: 'https://play.google.com/store/apps/details?id=com.gz.fortune',
-  appleStore:
-    'https://apps.apple.com/my/app/luckypot%E5%B9%B8%E6%B9%98%E8%8F%9C%E9%A6%86/id6753709372',
+  appleStore: 'https://apps.apple.com/my/app/id6766782701',
 
   // Platform indicator: 'APP' = Mobile (Capacitor/Native), 'WEB' = Browser (PWA)
-  platform: 'WEB' as 'APP' | 'WEB',
+  platform: 'APP' as 'APP' | 'WEB',
 
   // PWA version — bump this to force all web clients to reload
   pwaVersion: '1.3.6',
@@ -21,4 +20,3 @@ export const environment = {
   // OneSignal App ID (shared between Native and Web push)
   oneSignalAppId: '491eab16-2d96-4351-afb9-2186f4e07d7e',
 };
-
